5d0c2f494d5445f1d291346ef66a47618658020d
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2021-10-14 17:27:43 +0100

    changes

--- Week 1/empty-example/sketch.js
+++ Week 1/empty-example/sketch.js
@@ -1,12 +1,15 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1920, 1080, WEBGL);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  background(205, 102, 94);
+  rotateX(mouseX/360);
+  rotateY(mouseY/360);
+  sphere(360); 
+  fill(7, 67, 14);
+    if (mouseIsPressed) {
+      scale(1);
+    }
+
 }
