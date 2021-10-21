8273626fd69d56656905306d1373e2184134fa4c
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2021-10-21 16:52:29 +0100

    week 2 changes
    changing size of rectangle

---
+++ Week 2/tutorial-2/sketch.js
@@ -0,0 +1,20 @@
+let rectWidth = 500;
+let rectHeight = 250;
+
+function setup() {
+  createCanvas(1920,1080);
+  background(255);
+  //background colour 0 for black, 255 for white
+}
+
+function draw() {
+  //text("Hello World!", 0, 50);
+  //ellipse(50,50,40,40);
+  //line(0,50,400,70);
+  //triangle(347,54,392,9,392,66);
+  //quad(158,55,199,14,392,66,351,107);
+  //arc(300,300,50,50,90,270);
+  rect(25,50,rectWidth,rectHeight);
+  rect(155,444,rectWidth,rectHeight)
+
+}
