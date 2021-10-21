54b2fd1b1a29aae423ea0eb8760f89303fccf93b
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2021-10-21 17:44:01 +0100

    randomized the colour of the eyes

--- Week 2/tutorial-2/sketch.js
+++ Week 2/tutorial-2/sketch.js
@@ -45,18 +45,22 @@ function drawShape(rectangleWidth,rectangleHeight){
 function draw(){
   fill(random(0,255),random(0,255),random(0,255));
   drawShape(rectWidth,rectHeight);
+  fill(random(0,255),random(0,255),random(0,255));
   drawShape1(ellipseWidth,ellipseHeight);
 
   //weird alien drawing
+  fill(random(0,255),random(0,255),random(0,255));
   rectMode(CENTER);
   rect(100,100,20,100);
   ellipse(100,70,60,60);
-  ellipse(81,70,16,16);
-  ellipse(119,70,16,16);
   line(90,120,80,105);
   line(110,120,120,105);
   line(90,150,80,160);
   line(110,150,120,160);
+  //eyes
+  fill(random(0,255),random(0,255),random(0,255));
+  ellipse(81,70,16,16);
+  ellipse(119,70,16,16);
 
 }
 
