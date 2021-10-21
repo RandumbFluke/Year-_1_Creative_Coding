fb7ccb2b2d7dd0a2402d527a5ca1cf18c6af447a
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2021-10-21 17:41:53 +0100

    circle and rectangle

--- Week 2/tutorial-2/sketch.js
+++ Week 2/tutorial-2/sketch.js
@@ -1,33 +1,38 @@
 let rectWidth = 500;
 let rectHeight = 250;
 
+let ellipseWidth = 250;
+let ellipseHeight = 350;
+
 function setup() {
   createCanvas(1920,1080);
   background(255);
   //background colour 0 for black, 255 for white
-  //noLoop();
+  noLoop();
 }
 
-function draw() {
+//function draw() {
   //text("Hello World!", 0, 50);
   //ellipse(50,50,40,40);
   //line(0,50,400,70);
   //triangle(347,54,392,9,392,66);
   //quad(158,55,199,14,392,66,351,107);
   //arc(300,300,50,50,90,270);
-  rect(25,50,rectWidth,rectHeight+14);
-  rect(155,444,rectWidth-72,rectHeight);
-  rect(1000,600,rectWidth,rectHeight*0.5);
-  rect(540,250,rectWidth/2,rectHeight);
+  //rect(25,50,rectWidth,rectHeight+14);
+  //rect(155,444,rectWidth-72,rectHeight);
+  //rect(1000,600,rectWidth,rectHeight*0.5);
+  //rect(540,250,rectWidth/2,rectHeight);
 
-}
+//}
 
-function drawShape(ellipseWidth,ellipseHeight){
+//ellipse drawShape
+function drawShape1(ellipseWidth,ellipseHeight){
   let xPos = random(0, width);
   let yPos = random(0, height);
   ellipse(xPos,yPos,ellipseWidth,ellipseHeight);
 }
 
+//rectangle drawShape
 function drawShape(rectangleWidth,rectangleHeight){
   let xPos = random(0, width);
   let yPos = random(0, height);
@@ -40,7 +45,7 @@ function drawShape(rectangleWidth,rectangleHeight){
 function draw(){
   fill(random(0,255),random(0,255),random(0,255));
   drawShape(rectWidth,rectHeight);
-  drawShape(ellipseWidth,ellipseHeight);
+  drawShape1(ellipseWidth,ellipseHeight);
 
   //weird alien drawing
   rectMode(CENTER);
@@ -54,3 +59,4 @@ function draw(){
   line(110,150,120,160);
 
 }
+
