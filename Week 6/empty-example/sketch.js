ba516f928359bcc0f4bcf414710e0ea84aeb1a96
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2021-11-18 14:16:15 +0000

    week 6 task 1

---
+++ Week 6/empty-example/sketch.js
@@ -0,0 +1,15 @@
+let myArr= [];
+
+function setup() {
+  createCanvas (1024,400);
+  background(255);
+  for (let i = 0; i <= 255; i++){
+    myArr.push([i]);
+    console.log(myArr);
+  }
+}
+
+
+function draw() {
+  
+}
