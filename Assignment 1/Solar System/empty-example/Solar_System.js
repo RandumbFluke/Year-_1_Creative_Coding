624214c3ac08a911979801c847cc6d6eeee5a132
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2022-01-17 15:00:49 +0000

    made a references page, added comments and changed background colour

---
+++ Assignment 1/Solar System/empty-example/Solar_System.js
@@ -0,0 +1,7 @@
+function setup() {
+  createCanvas(1000,600); //created a canvas with 1000px on the x-axis and 600px on the y-axis
+}
+
+function draw() {
+  background("#191970"); //added the background colour midnightblue
+}
