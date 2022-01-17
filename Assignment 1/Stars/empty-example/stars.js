d688f6f417006abba2ba7dc13d95f27a4981f6be
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2022-01-17 13:58:48 +0000

    update

---
+++ Assignment 1/Stars/empty-example/stars.js
@@ -0,0 +1,10 @@
+function setup() {
+  let stars = []; //array of stars
+  createCanvas(800, 600); //created canvas dimensions 800px on the y-axis and 600px on the x-axis
+
+
+}
+
+function draw() {
+ background(0);
+}
