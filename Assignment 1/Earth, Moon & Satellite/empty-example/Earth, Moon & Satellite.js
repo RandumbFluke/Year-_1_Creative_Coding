580ee25b6980c11eec8b815a8d974096612826a5
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2022-01-18 13:42:06 +0000

    added satellite wing

--- Assignment 1/Earth, Moon & Satellite/empty-example/Earth, Moon & Satellite.js
+++ Assignment 1/Earth, Moon & Satellite/empty-example/Earth, Moon & Satellite.js
@@ -1,5 +1,6 @@
 function setup() {
   createCanvas(1000,800); //Create a rectangle canvas of 1000px by 800px
+  angleMode(DEGREES);
 }
 
 function draw() {
@@ -9,9 +10,6 @@ function draw() {
   strokeWeight(2); //Gave the ellipse more of an outline by increasing the stroke weight
   fill("#00BFFF");//Colour is DeekSkyBlue
   ellipse(500,400,400); //Ellipse in the middle of the page
-  strokeWeight(3); //Increased the stroke weight to add more depth
-  fill("#228B22");//Colour is ForestGreen
-  AbortController()
 
   //Moon
   strokeWeight(2); //Gave the ellipse more of an outline by increasing the stroke weight
@@ -25,5 +23,24 @@ function draw() {
   ellipse(780,240,40); //Small ellipse within the 'main' one
 
   //Satellite
+  strokeWeight(1);
+  fill("#DCDCDC");//Colour is Gainsboro
+  translate(240,180);
+  rotate(130);
+  rect(0,0,30,100);
+
+  //Satellite wing
+  strokeWeight(2);
+  fill(0);
+  line(30,20,40,20);
+  line(30,80,40,80);
+  line(0,20,-10,20);
+  line(0,80,-10,80);
+
+  strokeWeight(1);
+  fill("#191970");//Colour is MidnightBlue
+  translate(60,95);
+  rotate(180);
+  rect(0,0,20,90);
 
 }
