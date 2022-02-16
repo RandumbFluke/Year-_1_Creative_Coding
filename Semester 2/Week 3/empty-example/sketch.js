5283797c01061955e3ef262b0466857a55280cbb
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2022-02-16 10:08:35 +0000

    update

--- Semester 2/Week 3/empty-example/sketch.js
+++ Semester 2/Week 3/empty-example/sketch.js
@@ -2,7 +2,7 @@ let x,y;
 let radius = 100;
 let r,g,b;
 let timer = 10;
-let interval = 60;
+let interval = 30;
 let score = 0;
 let gameOver = false;
 
@@ -16,28 +16,51 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  noStroke();
-  fill(r,g,b);
-  ellipse(x, y, radius*2, radius*2);
-    if (frameCount % interval == 0 && timer > 0){
+  background(220);
+
+  if(!gameOver) {
+    noStroke();
+    fill(r, g, b);
+    ellipse(x, y, radius*2, radius*2);
+    textSize(24);
+    textAlign(LEFT, CENTER);
+    text("Score: " + score, 10, 30);
+    text("Countdown: " + timer, 120, 30);
+
+    if (frameCount % interval == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
       timer --;
       newCircle();
     }
-  textSize(24);
-  textAlign(LEFT, CENTER);
-  text("Score: " + score, 10,30);
-  text("Countdown: " + timer, 120,30);
+    if (timer === 0) {
+      
+      gameOver = true;
+    }
+  } else {
+    textSize(100);
+    textAlign(CENTER, CENTER);
+    text("GAME OVER", width/2, height/2);
+    
+  }
 }
 
-function mousePressed(){
-  let d = dist(mouseX, mouseY, x, y);
-    if(d < radius){
-      newCircle();
-      score++;
-      console.log("Score!");
+function mousePressed() {
+  
+  if(!gameOver) {
+      let d = dist(mouseX, mouseY, x, y);
+       if (d < radius) {
+          newCircle();
+          score++;
+        }
+    } else {
+      let d = dist(mouseX, mouseY, windowWidth/2,  windowHeight/2);
+      if (d < radius*2) {
+        gameOver = false;
+        timer = 10;
+        score = 0;
+      }
     }
 }
+
 
 function newCircle(){
   x = random(windowWidth);
