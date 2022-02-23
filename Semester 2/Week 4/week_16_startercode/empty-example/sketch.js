a01b44003e8a37db51518f52dba157a71f5578bb
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2022-02-23 14:48:14 +0000

    task 1 done

--- Semester 2/Week 4/week_16_startercode/empty-example/sketch.js
+++ Semester 2/Week 4/week_16_startercode/empty-example/sketch.js
@@ -1,13 +1,32 @@
-
+let bird;
+let pipes = []
+let pipeInterval = 120;
+let timer = 0;
+let timerInterval = 60;
+let level = 0;
+let health = 10;
 
 function setup() {
-
+    createCanvas(windowWidth, windowHeight);
+    bird = new Bird();
+    pipes.push(new Pipe());
+    noStroke();
 }
-
 
 function draw() {
-  
- 
+    background(220);
+    runGame(1);
 }
 
+function runGame(intervalScale){
+    bird.update();
+    bird.show();
+}
+
+function keyPressed(){
+    if (keyCode === 32){
+        bird.up();
+        console.log("SPACE");
+    }
+}
 
