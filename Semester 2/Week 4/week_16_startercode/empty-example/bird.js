8ce3be5f8a35cfe8c19bdc8a1ccd2e1c3bb6bd3e
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2022-02-23 14:39:54 +0000

    downloaded starter code

---
+++ Semester 2/Week 4/week_16_startercode/empty-example/bird.js
@@ -0,0 +1,44 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/cXgA1d_E-jY&
+
+class Bird  {
+
+  constructor() {
+    this.y = height/2;
+    this.x = 64;
+
+    this.gravity = 0.5;
+    this.lift = -14;
+    this.velocity = 0;
+  }
+  
+
+  show() {
+    noStroke();
+    fill("#ff884f");
+    ellipse(this.x, this.y, 32, 32);
+  }
+
+  up() {
+    this.velocity += this.lift;
+  }
+
+  update() {
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+
+    if (this.y > height) {
+      this.y = height;
+      this.velocity = 0;
+    }
+
+    if (this.y < 0) {
+      this.y = 0;
+      this.velocity = 0;
+    }
+
+  }
+
+}
