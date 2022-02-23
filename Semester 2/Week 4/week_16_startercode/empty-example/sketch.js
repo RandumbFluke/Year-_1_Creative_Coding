7812944ce124b9af0494017b8f5a21d0329e68d7
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2022-02-23 14:54:16 +0000

    task 2 done

--- Semester 2/Week 4/week_16_startercode/empty-example/sketch.js
+++ Semester 2/Week 4/week_16_startercode/empty-example/sketch.js
@@ -19,8 +19,25 @@ function draw() {
 }
 
 function runGame(intervalScale){
+    for (let i = pipes.length-1; i >= 0; i--){
+        pipes[i].show();
+        pipes[i].update();
+
+        if (pipes[i].hits(bird)){
+            console.log("HIT");
+        }
+
+        if (pipes[i].offscreen()){
+            pipes.splice(i, 1);
+        }
+    }
+
     bird.update();
     bird.show();
+
+    if (frameCount % pipeInterval === 0){
+        pipes.push(new Pipe());
+    }
 }
 
 function keyPressed(){
