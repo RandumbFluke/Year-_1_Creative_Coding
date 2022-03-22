7f3a44aac0c63e74d47c1777439641c74f0add95
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2022-03-22 15:09:37 +0000

    rotating vinyl

--- Semester 2/Assignment/empty-example/vinyl.js
+++ Semester 2/Assignment/empty-example/vinyl.js
@@ -1,30 +1,58 @@
+let angle = 0;
+
+function setup(){
+    createCanvas(800,800);
+    angleMode(DEGREES);
+}
+
 //Vinyl Background
 function drawshape_vinyl(){ 
     //Vinyl Track
     push();
+    //Rotation
+    translate(420,400);
+    rotate(angle);
+
     //Main circle
     fill(20);
     strokeWeight(1);
-    ellipse(420,400,250);
+    ellipse(0,0,250);
     //Rings
     strokeWeight(2);
     stroke("#808080");
-    ellipse(420,400,225);
-    ellipse(420,400,175);
+    ellipse(0,0,225);
+    ellipse(0,0,175);
     //Outline around middle
     strokeWeight(0);
     fill(0);
-    ellipse(420,400,125);
+    ellipse(0,0,125);
     //Middle of Vinyl
     fill(255,165,0);
-    ellipse(420,400,100);
+    ellipse(0,0,100);
+
+    //Design on track
+    fill(255,255,0);
+    ellipse(0,0,60);
+    fill(0);
+    ellipse(10,-10,10);
+    ellipse(-10,-10,10);
+    fill(255);
+    ellipse(10,-10,7);
+    ellipse(-10,-10,7);
+    fill(0);
+    ellipse(10,-10,3);
+    ellipse(-10,-10,3);
+    fill(255);
+    strokeWeight(2);
+    stroke(0);
+    ellipse(0,10,15);
+    
+    angle = angle + 1;
     pop();
   
     //Vinyl Cover
     push();
     fill(0);
-    //strokeWeight(1);
-    //stroke(255);
     rect(150,265,270);
     pop();
   
