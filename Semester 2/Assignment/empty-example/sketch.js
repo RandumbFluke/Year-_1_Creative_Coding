9f211c7c4b0da8d098f892db873cb52119dca8ab
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2022-04-05 17:19:35 +0100

    done first one

--- Semester 2/Assignment/empty-example/sketch.js
+++ Semester 2/Assignment/empty-example/sketch.js
@@ -2,21 +2,29 @@ let song1, song2, song3;
 let currentSong;
 let hasSongChanged = false;
 let lastKey;
+let amp;
+let amp2;
+let fft;
+let particles = [];
 
-function preload(){
-  song1 = loadSound("../Music/Tropic Love.mp3");
+function preload() {
+  song1 = loadSound("../Music/Omen.mp3");
   song2 = loadSound("../Music/Cartoon - Why We Lose.mp3");
   song3 = loadSound("../Music/Cartoon - On and On.mp3");
+  song4 = loadSound("../Music/Tropic Love.mp3");
 }
 
 function setup() {
-  createCanvas(800,800);
+  createCanvas(800, 800);
   getAudioContext().suspend();
   currentSong = song1;
-  //console.log(currentSong, song1);
-  currentSong.play();
   currentSong.setVolume(0.25);
-  //noLoop();
+  
+
+  amp = new p5.Amplitude();
+
+  angleMode(DEGREES);
+  fft = new p5.FFT();
 }
 
 //Draw function
@@ -26,59 +34,61 @@ function draw() {
   background(0);
   textAlign(CENTER);
   fill(255);
-  text('PRESS KEYS - 1, 2, 3, 4',width/2,height/2);
+  text('PRESS KEYS - 1, 2, 3, 4', width / 2, height / 2);
   pop();
 
 
   //Switch key
-  switch(key){
+  switch (key) {
     case "1":
       drawshape_speaker();
-    break;
+      break;
 
     case "2":
       drawshape_vinyl();
-    break;
+      break;
 
     case "3":
       drawshape_triplet();
-    break;
+      break;
 
     case "4":
       drawshape_particle_field();
-    break;
+      break;
   }
-  
- 
+
+
 }
 
 function keyPressed() {
   userStartAudio();
   hasSongChanged = true;//Switch key
-  switch(key){
+  switch (key) {
     case "1":
       currentSong.stop();
       currentSong = song1;
       currentSong.play();
-      
-    break;
+
+      break;
 
     case "2":
       currentSong.stop();
       currentSong = song2;
       currentSong.play();
-     
-    break;
+
+      break;
 
     case "3":
       currentSong.stop();
       currentSong = song3;
       currentSong.play();
-    break;
+      break;
 
     case "4":
-      
-    break;
+      currentSong.stop();
+      currentSong = song4;
+      currentSong.play();
+      break;
     default:
       currentSong.stop();
       break;
--- Semester 2/Assignment/empty-example/speaker.js
+++ Semester 2/Assignment/empty-example/speaker.js
@@ -1,30 +1,107 @@
 //Speaker
 function drawshape_speaker(){
-    //Background
-    background("#FF69B4");
+  background("#FF69B4");
 
-    //Speaker
-    push();
-    fill(20);
-    ellipse(400,400,350);
-    stroke(0);
-    strokeWeight(1);
-    fill(50);
-    ellipse(400,400,300);
-    fill(70);
-    ellipse(400,400,260);
-    fill(50);
-    ellipse(400,400,100);
-    fill(40);
-    ellipse(400,400,80);
-    fill(40);
-    ellipse(400,400,70);
-    fill(200);
-    ellipse(300,303,12);
-    ellipse(500,303,12);
-    ellipse(300,498,12);
-    ellipse(500,498,12);
-    pop();
+  push();
+  fill(255);
+  textSize(30);
+  text("Cartoon x Time To Talk - Omen (Ft. Asena)", 120, 100);
+  pop();
+
+  push();
+  let vol = amp.getLevel();
+  let diam = map(vol, 0, 0.25, 100, 200);
+  let diam2 = map(vol, 0, 0.25, 80, 180);
+  let diam3 = map(vol, 0, 0.25, 70, 150);
+
+
+  //Speaker
+  //Outside
+  fill(20);
+  ellipse(400, 400, 350);
+  stroke(0);
+  strokeWeight(1);
+  //Inner ring
+  fill(50);
+  ellipse(400, 400, 300);
+  //Screws
+  fill(200);
+  ellipse(300, 303, 12);
+  ellipse(500, 303, 12);
+  ellipse(300, 498, 12);
+  ellipse(500, 498, 12);
+  pop();
+
+
+  push();
+  fill(70);
+  ellipse(400, 400, 260);
+  fill(50);
+  ellipse(400, 400, diam, diam);
+  fill(40);
+  ellipse(400, 400, diam2, diam2);
+  fill(80);
+  ellipse(400, 400, diam3, diam3);
+  pop();
+
+
+  push();
+  noFill();
+  stroke(255);
+  translate(width / 2, height / 2);
+
+  fft.analyze();
+  amp2 = fft.getEnergy(20, 200);
+
+  let wave = fft.waveform();
+
+  let p = new Particle();
+  particles.push(p);
+
+  for (let i = particles.length - 1; i >= 0; i--) {
+    if (!particles[i].edges()) {
+      particles[i].update(amp2 > 150);
+      particles[i].show();
+    } else {
+      particles.splice(i, 1);
+    }
+
+  }
+
+  pop();
+  }
+
+  class Particle {
+    constructor(){
+      this.pos = p5.Vector.random2D().mult(180);
+      this.vel = createVector(0,0);
+      this.acc = this.pos.copy().mult(random(0.0001, 0.00001));
+  
+      this.w = random(3, 5);
+    }
+    update(cond){
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      if (cond){
+        this.pos.add(this.vel);
+        this.pos.add(this.vel);
+        this.pos.add(this.vel);
+      }
+    } 
+  
+    edges(){
+      if (this.pos.x < -width /2 || this.pos.x > width / 2 || this.pos.y < -height / 2 || this.pos.y > height / 2){
+        return true;
+      } else {
+        return false;
+      }
+    }
+    
+    show(){
+      noStroke();
+      fill(255);
+      ellipse(this.pos.x, this.pos.y, this.w);
+    }
   }
 
   
